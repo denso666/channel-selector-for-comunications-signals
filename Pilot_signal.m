f27ce9cd9814cb18ee76cf680f09169ccc245f11
Guminola <89585270+Guminola@users.noreply.github.com>
2022-06-15 09:17:47 -0500

    Add files via upload
    fixed stabilty issues

--- Pilot_signal.m
+++ Pilot_signal.m
@@ -1,13 +1,13 @@
 clc; 
 
 %% ****** Constant variables ***** %%
-chanels = 5;
+chanels = 1;
 frequency = 1;
 stability = 30;
 amplitude = 20;
 
-%% ****** Pilot and noice signals ***** %%
-[m_t,t] = pilot_signal(amplitude, frequency);
+%% ****** Pilot and noise signals ***** %%
+[m_t,t] = pilot_signal(amplitude, frequency, stability);
 m_t = binary(m_t, 1, stability);
 R = noise(m_t, chanels, stability);
 R = binary(R, chanels, stability);
@@ -23,13 +23,13 @@ Pn = Ps ./ SNR;
 An = sqrt(Pn);
 
 %% ****** Binary values ***** %%
-%m_t = dec2bin(m_t*100, 16);			% 11b
-%R = dec2bin(m_t*100, 16);			% 13b
-%Ps = dec2bin(Ps*100, 16);			% 15b
-%Pn = dec2bin(Pn*100, 16);			% 15b
-%SNRdb = dec2bin(SNRdb*100, 16);		% 8b
-%SNR = dec2bin(SNR*100, 16);			% 7b
-%An = dec2bin(An*100, 16);			% 11b
+m_t = dec2bin(m_t*100, 16);			% 11b
+R = dec2bin(m_t*100, 16);			% 13b
+Ps = dec2bin(Ps*100, 16);			% 15b
+Pn = dec2bin(Pn*100, 16);			% 15b
+SNRdb = dec2bin(SNRdb*100, 16);		% 8b
+SNR = dec2bin(SNR*100, 16);			% 7b
+An = dec2bin(An*100, 16);			% 11b
 
 %% ****** Ploting signals ***** %%
 %figure(1)
@@ -42,34 +42,33 @@ An = sqrt(Pn);
 %xlabel('Time (s)'); ylabel('Voltage')
 %grid on
 
-
 clear t stability chanels frequency amplitude;
 
 %% ****** Private funcions ***** %%
-function [m_t,time] = pilot_signal(amplitude, frequency)
-	time = 0:(1/29):1; 			%Time vector (30 samples)
+function [signal,time] = pilot_signal(amplitude, frequency, stability)
+	time = 0:(1/(stability-1)):1; 			%Time vector (30 samples)
 	w = 2*pi*frequency; 		%Omega value	
-	m_t = amplitude*cos(w*time); %Cosine signal
+	signal = amplitude*cos(w*time); %Cosine signal
 end 
 
-function R = noise(m_t, chanels, stability)
-	n = zeros(chanels,stability);	%noice matrix
-	R = zeros(chanels,stability);
+function noise_signal = noise(m_t, chanels, stability)
+	just_noise = zeros(chanels,stability);	%noise matrix
+	noise_signal = zeros(chanels,stability);
 
 	for i=1 : 1 : chanels
-		n(i, :) = rand(1,30)*2-1;
+		just_noise(i, :) = rand(1,stability)*2-1;
 	end
 
 	for i=1 : 1 : chanels
-		R(i, :) = n(i, :) + m_t;
+		noise_signal(i, :) = just_noise(i, :) + m_t;
 	end
 end
 
-function bit = binary(dec, chanels, stability)
-	bit = abs(dec) * 10000;
-	bit = dec2bin(bit,16);
-	bit = bin2dec(bit);
-	bit = bit/10000;
+function dec_2_bin = binary(decimal, chanels, stability)
+	dec_2_bin = abs(decimal) * 10000;
+	dec_2_bin = dec2bin(dec_2_bin,16);
+	dec_2_bin = bin2dec(dec_2_bin);
+	dec_2_bin = dec_2_bin/10000;
 
-	bit = reshape(bit, chanels, stability);
+	dec_2_bin = reshape(dec_2_bin, chanels, stability);
 end
